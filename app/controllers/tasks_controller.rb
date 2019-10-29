c81d1cbd59064efd1929c8f08c5b3d099b58e47c
vgeroim <vgeroim.qa@gmail.com>
2019-10-29 03:08:17 +0000

    add task creation api

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,12 @@ class TasksController < ApplicationController
     render json: task
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:title,:done)
   end
 end
