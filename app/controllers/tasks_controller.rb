6e933ebe5e103604ce7554faa41faf95796a6aad
vgeroim <vgeroim.qa@gmail.com>
2019-10-29 00:55:23 +0000

    add update method to Task controller for status update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def index
     render json: Task.all
   end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task
+  end
+
+  def task_params
+    params.require(:task).permit(:done)
+  end
 end
